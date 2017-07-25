d1221a74dc357b439b666af50a5a5f466097f517
Edu Santos Paz <eduardo@edsantoshn.com>
2017-07-25 10:39:58 -0600

    Numeros Perfectos

---
+++ numerosperfectos.py
@@ -0,0 +1,30 @@
+class numerosPerfectos():
+	def __init__(self):
+		print('Bienvenido al modulo de numeros perfectos')
+
+	def inicio (self):
+		numero_ciclos = int(input('Ingrese la cantidad de ciclos que desea obtener los numeros perfectos: '))
+		resultado_ciclos_uno = numeros_perfectos(numero_ciclos)
+		imprimir_numeros_perfectos_listado(resultado_ciclos_uno)
+
+	def numeros_perfectos(self,n):
+		x=n-1
+		resultado_uno = 2**x
+		resultado_dos = (2**n)-1
+		resultado_tres = resultado_uno * resultado_dos
+		return resultado_tres
+
+	def numeros_perfectos_ciclos(self,ciclos):
+		listado_numeros_perfectos = []
+		for elemento in range((ciclos+1)):
+			resultado_ciclos= numeros_perfectos(elemento)
+			listado_numeros_perfectos.append(resultado_ciclos)
+		return listado_numeros_perfectos
+
+	def imprimir_numeros_perfectos_listado(self,listado):
+		height = len(listado)
+		if(height>0):
+			for elemento in range(height):
+				print(listado[elemento])
+		else:
+			print('La lista esta vacia')
